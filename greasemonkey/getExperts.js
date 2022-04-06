be076b0b8095b989dc7aa818d58d3ff349827d18
JoshuaHenriques <joshuahenriques95@gmail.com>
2022-04-06 14:05:39 -0400

    bookmarklet ready

--- greasemonkey/getExperts.js
+++ greasemonkey/getExperts.js
@@ -1,31 +1,31 @@
-// let company = "google"
-// let companyUUID = "6acfa7da-1dbd-936e-d985-cf07a1b27711"
-// let company = "farmart"
-// let companyUUID = "c30e2e2c-2873-ef6f-c5f1-a2874f20f66f"
-// let company = "amazon"
-// let companyUUID = "05554f65-6aa9-4dd1-6271-8ce2d60f10c4"
-// let company = "apollo-global-management-llc"
-// let companyUUID = "84c5275e-2a2f-e43a-1ff1-1a5f7c79975f"
-// let company = "nvidia"
-// let companyUUID = "ee17319e-f5ee-9c9a-6500-edf82b4fbf05"
-// let company = "grofers-trusted-delivery-partner"
-// let companyUUID = "79ec9579-eca9-0029-e080-a43ebfea7389"
-// let company = "intel"
-// let companyUUID = "1e4f199c-363b-451b-a164-f94571075ee5"
-// let company = "tencent"
-// let companyUUID = "3cae090b-ed2d-95f8-79a9-e32ca480258f"
-// let company = "apple"
-// let companyUUID = "7063d087-96b8-2cc1-ee88-c221288acc2a"
-// let company = "angellist"
-// let companyUUID = "55d02c5a-f0d6-f70f-1a04-832633cfce2a"
+// var company = "google"
+// var companyUUID = "6acfa7da-1dbd-936e-d985-cf07a1b27711"
+// var company = "farmart"
+// var companyUUID = "c30e2e2c-2873-ef6f-c5f1-a2874f20f66f"
+// var company = "amazon"
+// var companyUUID = "05554f65-6aa9-4dd1-6271-8ce2d60f10c4"
+// var company = "apollo-global-management-llc"
+// var companyUUID = "84c5275e-2a2f-e43a-1ff1-1a5f7c79975f"
+// var company = "nvidia"
+// var companyUUID = "ee17319e-f5ee-9c9a-6500-edf82b4fbf05"
+// var company = "grofers-trusted-delivery-partner"
+// var companyUUID = "79ec9579-eca9-0029-e080-a43ebfea7389"
+// var company = "intel"
+// var companyUUID = "1e4f199c-363b-451b-a164-f94571075ee5"
+// var company = "tencent"
+// var companyUUID = "3cae090b-ed2d-95f8-79a9-e32ca480258f"
+// var company = "apple"
+// var companyUUID = "7063d087-96b8-2cc1-ee88-c221288acc2a"
+// var company = "angellist"
+// var companyUUID = "55d02c5a-f0d6-f70f-1a04-832633cfce2a"
 
 // regex for uuid: /semrush_summary&q;:{&q;identifier&q;:{&q;uuid&q;:&q;(\w{8}(-\w{4}){3}-\w{12}?)/g
 // uuid: document.querySelector("#client-app-state").textContent.match(/semrush_summary&q;:{&q;identifier&q;:{&q;uuid&q;:&q;(\w{8}(-\w{4}){3}-\w{12}?)/g)[0].split(";")[6]
 // company: window.location.pathname.split("/")[2] or document.URL.split("/")[4]
-let company = document.URL.split("/")[4]
-let companyUUID = document.querySelector("#client-app-state").textContent.match(/semrush_summary&q;:{&q;identifier&q;:{&q;uuid&q;:&q;(\w{8}(-\w{4}){3}-\w{12}?)/g)[0].split(";")[6]
+var company = document.URL.split("/")[4]
+var companyUUID = document.querySelector("#client-app-state").textContent.match(/semrush_summary&q;:{&q;identifier&q;:{&q;uuid&q;:&q;(\w{8}(-\w{4}){3}-\w{12}?)/g)[0].split(";")[6]
 
-let expertsList = []
+var expertsList = []
 
 const pushToExpertList = (data) => {
     data.entities.forEach((expert) => {
@@ -55,8 +55,8 @@ const sendToDB = async () => {
 }
 
 const fetchTheRest = async (last, cnt) => {
-    for (let i = 0; i < cnt; i++) {
-        let fetchData = await fetch("https://www.crunchbase.com/v4/data/searches/contacts?source=profile-contacts-card", {
+    for (var i = 0; i < cnt; i++) {
+        var fetchData = await fetch("https://www.crunchbase.com/v4/data/searches/contacts?source=profile-contacts-card", {
             "credentials": "include",
             "headers": {
                 "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0",
@@ -75,7 +75,7 @@ const fetchTheRest = async (last, cnt) => {
             "method": "POST",
             "mode": "cors"
         })
-        let data = await fetchData.json()
+        var data = await fetchData.json()
         if (data.entities.length < 10) {
             pushToExpertList(data)
             return
@@ -87,7 +87,7 @@ const fetchTheRest = async (last, cnt) => {
 }
 
 const grabExperts = async () => {
-    let fetchData = await fetch("https://www.crunchbase.com/v4/data/searches/contacts?source=profile-contacts-card", {
+    var fetchData = await fetch("https://www.crunchbase.com/v4/data/searches/contacts?source=profile-contacts-card", {
         "credentials": "include",
         "headers": {
             "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0",
@@ -107,23 +107,29 @@ const grabExperts = async () => {
         "mode": "cors"
     })
 
-    let data = await fetchData.json()
+    var data = await fetchData.json()
     if (data.entities.length < 10) {
         pushToExpertList(data)
         return
     } else {
-        let lastPersonID = data.entities[9].uuid
-        let loopCnt = Math.ceil(data.count / 10)
+        var lastPersonID = data.entities[9].uuid
+        var loopCnt = Math.ceil(data.count / 10)
 
         pushToExpertList(data)
         await fetchTheRest(lastPersonID, loopCnt)
     }    
 }
 
-const test = async () => {
+const test = () => {
     console.log(expertsList)
+    alert("done")
 }
 
-await grabExperts()
-await test()
+const run = async () => {
+    await grabExperts()
+    test()
+}
+run()
+// await grabExperts()
+// test()
 // await sendToDB()
